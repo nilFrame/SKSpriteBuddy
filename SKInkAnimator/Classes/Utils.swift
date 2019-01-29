1946954677b4238459e31f2aca4db83f9699bad6
Rafael Moura <rafael.moura@farfetch.com>
2019-01-29 10:57:04 +0000

    Updates podspec and Utils

--- SKInkAnimator/Classes/Utils.swift
+++ SKInkAnimator/Classes/Utils.swift
@@ -58,14 +58,20 @@ public struct IAXMLConstants {
 }
 
 public extension CGPoint {
+
     init?(xmlElement: AEXMLElement) {
+
         guard let xString = xmlElement.attributes["x"], let xFloat = Float(xString) else { return nil }
         guard let yString = xmlElement.attributes["y"], let yFloat = Float(yString) else { return nil }
+
+        self.init()
+
         self.x = CGFloat(xFloat)
         self.y = CGFloat(yFloat)
     }
     
     func xmlElement() -> AEXMLElement {
+
         let attributes = ["x": "\(self.x)", "y": "\(self.y)"]
         return AEXMLElement(name: "point", value: nil, attributes: attributes)
     }
@@ -73,53 +79,65 @@ public extension CGPoint {
 
 public extension CGFloat {
     init?(xmlElement: AEXMLElement) {
+
         guard let valueString = xmlElement.attributes["value"], let value = Float(valueString) else { return nil }
         self.init(value)
     }
     
     func xmlElement() -> AEXMLElement {
+
         let attributes = ["value": "\(self)"]
         return AEXMLElement(name: "float", value: nil, attributes: attributes)
     }
 }
 
 public extension String {
+
     func toBool() -> Bool? {
+
         switch self {
+
         case "True", "true":
             return true
+
         case "False", "false":
             return false
+
         default:
             return nil
         }
     }
     
     func toUUID() -> NSUUID? {
+
         return NSUUID(uuidString: self)
     }
     
     func toCGFloat() -> CGFloat?{
         
-        guard let floatValue = Float(self) else{
-            return nil
-        }
+        guard let floatValue = Float(self) else{ return nil }
         
         return CGFloat(floatValue)
-        
     }
 }
 
 
 public extension CGSize {
+
     init?(xmlElement: AEXMLElement) {
+
         guard let widthString = xmlElement.attributes["width"], let widthFloat = Float(widthString) else { return nil }
+
         guard let heightString = xmlElement.attributes["height"], let heightFloat = Float(heightString) else { return nil }
+
+        self.init()
+
         self.width = CGFloat(widthFloat)
         self.height = CGFloat(heightFloat)
     }
     
     func xmlElement() -> AEXMLElement {
+
         let attributes = ["width": "\(self.width)", "height": "\(self.height)"]
         return AEXMLElement(name: "size", value: nil, attributes: attributes)
     }
